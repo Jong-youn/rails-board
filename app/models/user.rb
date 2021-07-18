871d3dcf52f243c9dcad3076e4a026725805d490
ljy6816@gmail.com <ljy6816@gmail.com>
2021-07-18 20:18:02 +0900

    Update association between user and article

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :role
+  has_many :articles
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :nickname,  presence: true, 
+                        uniqueness: true, 
+                        length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, 
+                    uniqueness: true, 
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
