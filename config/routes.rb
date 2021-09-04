2befbf4cd4ce6476044c447f9a6fa46226bd33d7
ljy6816@gmail.com <ljy6816@gmail.com>
2021-09-04 23:35:34 +0900

    Remove Devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
-  devise_for :users
 
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
