a31361bc2c2ad50d90b8d13058fbc3f3ec181b69
ljy6816@gmail.com <ljy6816@gmail.com>
2021-09-05 11:26:51 +0900

    Add login - new

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'signup', to: 'users#new'
-  # post 'users', to: 'users#create'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
