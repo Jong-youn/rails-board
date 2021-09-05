b83b56aa6c91d8c48e647d22c57dabef216dfd7b
ljy6816@gmail.com <ljy6816@gmail.com>
2021-09-05 17:29:44 +0900

    Add admin column and feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_022937) do
+ActiveRecord::Schema.define(version: 2021_09_05_075945) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_09_05_022937) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "nickname"
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
   end
